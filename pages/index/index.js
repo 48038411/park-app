7d9a2f1e144ecaa80b7f2410e370381a6aa2575c
Guorc <48038411@qq.com>
2021-03-27 12:04:29 +0800

    引入腾讯地图sdk，使用腾讯地图sdk获取当前位置，修复不准确的 问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,8 @@
 // index.js
 // 获取应用实例
+// 引入SDK核心类，js文件根据自己业务，位置可自行放置
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var qqmapsdk;
 const app = getApp()
 
 Page({
@@ -13,8 +16,10 @@ Page({
     var that=this;
     wx.getLocation({
       //返回gps坐标，gcj02可返回用于openLocation函数的坐标，我这里不去，只展示
-      type: 'wgs84',
+      type: 'gcj02',
       success (res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
         that.setData({
           latitude:res.latitude,
           longitude:res.longitude,
@@ -22,7 +27,9 @@ Page({
             iconPath: "/images/location.png",
             id: 0,
             latitude: res.latitude,
-            longitude: res.longitude
+            longitude: res.longitude,
+            width:30,
+            height:30
           }],
         });
       }
@@ -30,6 +37,10 @@ Page({
   },
   onLoad: function (options) {
     var that=this;
+      // 实例化API核心类
+      qqmapsdk = new QQMapWX({
+        key: 'FSLBZ-WIXKX-UXY4I-TUO64-3MNZT-UKBEX'
+    });
     //获取用户当前的授权状态
     wx.getSetting({
       success(res) {
@@ -41,7 +52,6 @@ Page({
             //用户同意授权
             success () {
               // 用户已经同意小程序使用地理位置，后续调用 wx.getLocation 接口不会弹窗询问
-              that.getMyLocation();
             },
             //用户不同意授权
             fail(){
@@ -78,4 +88,55 @@ Page({
       }
     })
   },
+  onShow(){
+    var that = this;
+    wx.getLocation({
+      type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          latitude:res.latitude,
+          longitude:res.longitude,
+          markers: [{
+            iconPath: "/images/location.png",
+            id: 0,
+            latitude: res.latitude,
+            longitude: res.longitude,
+            width:30,
+            height:30
+          }],
+        });
+        qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: that.data.latitude,
+            longitude: that.data.longitude
+          },
+          success: function (res) {
+            console.log(res.result.address)
+            console.log(res.result.address_component.city)
+            console.log(res.result.address_component.district)
+            console.log(res.result.address_component.nation)
+            console.log(res.result.address_component.province)
+            console.log(res.result.address_component.street)
+            console.log(res.result.address_component.street_number)
+            console.log(res.result.formatted_addresses.recommend)
+
+
+
+            console.log("获取地址成功：" + res.result.ad_info.city);
+          },
+          fail: function (res) {
+            console.log("逆向解析地理位置服务异常,获取地址失败" + res);
+          },
+          complete: function (res) {
+            console.log(res);
+          }
+        });
+      }, 
+      fail:function(e){
+        console.log("由于未授权地理位置服务,获取地址失败" + e);
+        // that.setData({ isFixed: 'fixed', allowLayer:false});
+      }
+    　})
+  }
 })
