97a35e3e68dd512d7bfc2cfa5010afddc4f4f32f
Guorc <48038411@qq.com>
2021-03-28 18:56:24 +0800

    更换使用高德地图定位获取当前位置

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,44 +2,83 @@
 // 获取应用实例
 // 引入SDK核心类，js文件根据自己业务，位置可自行放置
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var amapFile = require('../../libs/amap-wx');
 var qqmapsdk;
 const app = getApp()
 
 Page({
   data: {
-    longitude:"",
-    latitude:"",
-    markers:[]
+    longitude: "",
+    latitude: "",
+    markers: [],
+    gaodeAddress: ""
   },
-  // 获取用户信息
-  getMyLocation:function(){
-    var that=this;
-    wx.getLocation({
-      //返回gps坐标，gcj02可返回用于openLocation函数的坐标，我这里不去，只展示
-      type: 'gcj02',
-      success (res) {
-        console.log(res.longitude)
-        console.log(res.latitude)
+  //高德地图获取定位
+  //通过两点经纬度计算距离（KM）  
+
+  distance: function (la1, lo1, la2, lo2) {
+    var La1 = la1 * Math.PI / 180.0;
+
+    var La2 = la2 * Math.PI / 180.0;
+
+    var La3 = La1 - La2;
+
+    var Lb3 = lo1 * Math.PI / 180.0 - lo2 * Math.PI / 180.0;
+
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(La3 / 2), 2) + Math.cos(La1) * Math.cos(La2) * Math.pow(Math.sin(Lb3 / 2), 2)));
+
+    s = s * 6378.137; //地球半径
+
+    s = Math.round(s * 10000) / 10000;
+
+    console.log("计算结果", s);
+
+    return s;
+
+  },
+  gaodeGetLocation: function () {
+    var that = this;
+
+    var myAmapFun = new amapFile.AMapWX({
+      key: '4ca3660bbd1986f8acc638ba8bf78e7e'
+    });
+
+    myAmapFun.getRegeo({
+      success: (res) => {
+        console.log("高德地图获取定位:" + JSON.stringify(res), res[0].regeocodeData.formatted_address, res[0].longitude);
+
+        //计算两个经纬度距离
+
         that.setData({
-          latitude:res.latitude,
-          longitude:res.longitude,
+          longitude: res[0].longitude,
+          latitude: res[0].latitude,
           markers: [{
             iconPath: "/images/location.png",
             id: 0,
-            latitude: res.latitude,
-            longitude: res.longitude,
-            width:30,
-            height:30
+            latitude: res[0].latitude,
+            longitude: res[0].longitude,
+            width: 30,
+            height: 30
           }],
-        });
+        })
+        var jl = that.distance(32.101352, 118.945995, res[0].latitude, res[0].longitude);
+
+        console.log("打印计算两个点的距离:" + jl);
+
+        this.setData({
+          gaodeAddress: res[0].regeocodeData.formatted_address
+        })
+
       }
-     })
+
+    })
+
   },
   onLoad: function (options) {
-    var that=this;
-      // 实例化API核心类
-      qqmapsdk = new QQMapWX({
-        key: 'FSLBZ-WIXKX-UXY4I-TUO64-3MNZT-UKBEX'
+    var that = this;
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: 'FSLBZ-WIXKX-UXY4I-TUO64-3MNZT-UKBEX'
     });
     //获取用户当前的授权状态
     wx.getSetting({
@@ -50,93 +89,45 @@ Page({
           wx.authorize({
             scope: 'scope.userLocation',
             //用户同意授权
-            success () {
+            success() {
               // 用户已经同意小程序使用地理位置，后续调用 wx.getLocation 接口不会弹窗询问
             },
             //用户不同意授权
-            fail(){
-                wx.showModal({
-                  title: '提示',
-                  content: '此功能需获取位置信息，请授权',
-                  success: function (res) {
-                    if (res.confirm == false) {
-                      return false;
-                    }
-                    wx.openSetting({
-                      success(res) {
-                        //如果再次拒绝则返回页面并提示
-                        if (!res.authSetting['scope.userLocation']) {
-                          wx.showToast({
-                            title: '此功能需获取位置信息，请重新设置',
-                            duration: 3000,
-                            icon: 'none'
-                          })
-                        } else {
-                          //允许授权，调用地图
-                          that.onLoad()
-                        }
-                      }
-                    })
+            fail() {
+              wx.showModal({
+                title: '提示',
+                content: '此功能需获取位置信息，请授权',
+                success: function (res) {
+                  if (res.confirm == false) {
+                    return false;
                   }
-                }) 
+                  wx.openSetting({
+                    success(res) {
+                      //如果再次拒绝则返回页面并提示
+                      if (!res.authSetting['scope.userLocation']) {
+                        wx.showToast({
+                          title: '此功能需获取位置信息，请重新设置',
+                          duration: 3000,
+                          icon: 'none'
+                        })
+                      } else {
+                        //允许授权，调用地图
+                        that.onLoad()
+                      }
+                    }
+                  })
+                }
+              })
             }
           })
-        }
-        else{
-          that.getMyLocation();
+        } else {
+          that.gaodeGetLocation()
         }
       }
     })
   },
-  onShow(){
-    var that = this;
-    wx.getLocation({
-      type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-      success: function (res) {
-        console.log(res)
-        that.setData({
-          latitude:res.latitude,
-          longitude:res.longitude,
-          markers: [{
-            iconPath: "/images/location.png",
-            id: 0,
-            latitude: res.latitude,
-            longitude: res.longitude,
-            width:30,
-            height:30
-          }],
-        });
-        qqmapsdk.reverseGeocoder({
-          location: {
-            latitude: that.data.latitude,
-            longitude: that.data.longitude
-          },
-          success: function (res) {
-            console.log(res.result.address)
-            console.log(res.result.address_component.city)
-            console.log(res.result.address_component.district)
-            console.log(res.result.address_component.nation)
-            console.log(res.result.address_component.province)
-            console.log(res.result.address_component.street)
-            console.log(res.result.address_component.street_number)
-            console.log(res.result.formatted_addresses.recommend)
+  onShow() {
+    this.gaodeGetLocation()
 
-
-
-            console.log("获取地址成功：" + res.result.ad_info.city);
-          },
-          fail: function (res) {
-            console.log("逆向解析地理位置服务异常,获取地址失败" + res);
-          },
-          complete: function (res) {
-            console.log(res);
-          }
-        });
-      }, 
-      fail:function(e){
-        console.log("由于未授权地理位置服务,获取地址失败" + e);
-        // that.setData({ isFixed: 'fixed', allowLayer:false});
-      }
-    　})
   }
 })
